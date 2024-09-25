14e89e8e3cfa8f2cb704becc188e5c02a4d0537c
LaughG33k <user@MacBook-Pro.local>
2024-09-25 14:31:44 +0200

    add new model

--- iternal/model/wsMessage.go
+++ iternal/model/wsMessage.go
@@ -8,15 +8,21 @@ type WsMessage struct {
 type Body101 struct {
 	Text      string `json:"text"`
 	Receiver  string `json:"recipient"`
-	MessageId string `json:"message_id"`
+	MessageId string `json:"messageId"`
 }
 
 type Body103 struct {
-	WithWhom string `json:"with_whom"`
+	WithWhom string `json:"withWhom"`
 }
 
 type Body104 struct {
-	MessageId     string `json:"message_id"`
-	WithWhom      string `json:"with_whom"`
-	FlagDelForEvr string `json:"flag_del_for_evr"`
+	MessageId     string `json:"messageId"`
+	WithWhom      string `json:"withWhom"`
+	FlagDelForEvr bool   `json:"flagDelForEvr"`
+}
+
+type Body105 struct {
+	MessageId   string `json:"messageId"`
+	WithWhom    string `json:"withWhom"`
+	UpdatedText string `json:"updatedText"`
 }
